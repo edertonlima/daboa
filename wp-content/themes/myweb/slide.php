a40e9d5534bc0643282dcd09f73db4606984a779
Ederton Lima <edertonlima@users.noreply.github.com>
2019-11-20 01:34:51 -0300

    Ajustes Mobile

--- wp-content/themes/myweb/slide.php
+++ wp-content/themes/myweb/slide.php
@@ -6,28 +6,35 @@
 			<?php 
 				if(is_home()){
 					$images = get_field('slide_principal',9);
+					//echo '<pre>home</pre>';
 				}else{
 					if(is_category()){
 						$images = get_field('slide_principal',$current_category->taxonomy.'_'.$current_category->term_id);
+						//var_dump($images);
+						//echo '<pre>category</pre>';
 					}else{
 						if((is_archive()) and (!is_tax())){
 							$images = get_field('slide_receitas','option');
+							//echo '<pre>archive y tax</pre>';
 						}else{
 							if(is_tax()){
 								$images = get_field('slide_principal',$current_category->taxonomy.'_'.$current_category->term_id);
+								//echo '<pre>tax</pre>';
 							}else{
 								$images = get_field('slide_principal');
+								//echo '<pre>normal</pre>';
 							}
 						}
 					}
 				}
+				//var_dump($images);
 			$qtd_slide = 0;
 			if( $images ): ?>
-		        <?php foreach( $images as $image ): //var_dump($image['imagem_slide_receitas']);
+		        <?php foreach( $images as $image ): //var_dump($image);
 		        	?>
 
 		        	<?php
-		        		if((is_archive()) and (!is_tax())){ ?>
+		        		if(((is_archive()) and (!is_tax())) and (!is_category())){ ?>
 
 			        		<div class="carousel-item<?php if($qtd_slide == 0){ echo ' active'; } ?>" style="background-image: url('<?php echo esc_url($image['imagem_slide_receitas']['sizes']['detalhe-post-page']); ?>');">
 			        			
@@ -36,7 +43,7 @@
 					        	<?php } ?>
 			        		</div>
 
-			        	<?php }else{ ?>
+			        	<?php }else{ //var_dump($image['imagem_slide_principal']['sizes']); ?>
 
 			        		<div class="carousel-item<?php if($qtd_slide == 0){ echo ' active'; } ?>" style="background-image: url('<?php echo esc_url($image['imagem_slide_principal']['sizes']['detalhe-post-page']); ?>');">
 			        			
@@ -53,9 +60,13 @@
 		</div>
 
 		<ol class="carousel-indicators <?php if((is_category()) or (is_tax())){ echo 'indicators-category'; } ?>">
-			<?php for($i=0; $i < $qtd_slide; $i++) { ?>		
-				<li data-target="#slide-home" data-slide-to="<?php echo $i; ?>"<?php if($i == 0){ echo ' class="active"'; } ?>></li>
-			<?php } ?>				
+			<?php
+				if($qtd_slide > 1){
+					for($i=0; $i < $qtd_slide; $i++) { ?>		
+						<li data-target="#slide-home" data-slide-to="<?php echo $i; ?>"<?php if($i == 0){ echo ' class="active"'; } ?>></li>
+					<?php }
+				}
+			?>				
 		</ol>
 
 		<a class="carousel-control-prev" href="#slide-home" role="button" data-slide="prev">
